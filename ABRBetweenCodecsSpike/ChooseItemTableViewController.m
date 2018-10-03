6cd066678c7769eb26950a6fad02288949972c14
Thomas Sherwood <thomas.sherwood@bbc.co.uk>
2018-10-03 13:55:44 +0100

    Add a few buttons to make selecting some stations easier

--- ABRBetweenCodecsSpike/ChooseItemTableViewController.m
+++ ABRBetweenCodecsSpike/ChooseItemTableViewController.m
@@ -9,7 +9,20 @@
 #import "ChooseItemTableViewController.h"
 
 @implementation ChooseItemTableViewController {
+    NSArray<NSString *> *_predefinedPlaylistURLs;
     __weak IBOutlet UITextField *_playlistURLTextField;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _predefinedPlaylistURLs = @[@"http://as-hls-uk-live.akamaized.net/pool_6/live/bbc_radio_one/bbc_radio_one.isml/.m3u8",
+                                @"http://as-dash-uk-live.akamaized.net/pool_7/live/bbc_radio_two/bbc_radio_two.isml/.m3u8",
+                                @"http://as-dash-uk-live.akamaized.net/pool_7/live/bbc_radio_three/bbc_radio_three.isml/.m3u8",
+                                @"http://as-dash-uk-live.akamaized.net/pool_6/live/bbc_radio_fourfm/bbc_radio_fourfm.isml/.m3u8",
+                                @"http://as-dash-uk-live.akamaized.net/pool_6/live/bbc_radio_five_live/bbc_radio_five_live.isml/.m3u8"
+                                ];
 }
 
 - (NSURL *)playlistURL
@@ -18,6 +31,12 @@
     NSURL *playlistURL = [NSURL URLWithString:textFieldContents];
     
     return playlistURL;
+}
+
+- (IBAction)radioButtonTapped:(UIButton *)sender
+{
+    NSString *selectedURL = _predefinedPlaylistURLs[sender.tag];
+    _playlistURLTextField.text = selectedURL;
 }
 
 @end
